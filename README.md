7191852ffd0cae5f303519fecb5af0cba52e0704
Enrique Casanova <enrique.casanova@projekt202.com>
2022-07-12 17:47:41 -0500

    Add support to aws multi region and profile

--- README.md
+++ README.md
@@ -8,11 +8,12 @@ In order to use this script you should have Python 2 or Python 3 installed on yo
 
 To start export proccess you shout run next command (**Note**: use `python3` if you have Python 3 instaled)
 
-- `$ python3 CognitoExport.py --user-pool-id 'us-east-2_XXXXXXX' --user-new-pool-id 'us-east-2_XXXXXXXX' --region us-east-2 -groups TestGroup`
+- `$ python3 CognitoExport.py --user-pool-id 'us-east-2_XXXXXXX' --user-new-pool-id 'us-east-2_XXXXXXXX' --region-current-pool us-east-2 --region-new-pool us-east-2 -groups TestGroup`
 
 ### Script Arguments
 
 - `--user-pool-id` [__Required__] - The user pool ID for the user pool on which the export should be performed
-- `--region` [_Optional_] - The user pool region the user pool on which the export should be performed _Default_: `us-east-1`
+- `--region-current-pool` [_Optional_] - The user pool region the user pool on which the export should be performed _Default_: `us-east-1`
 - `--user-new-pool-id` [__Required__] - The user pool ID for the user pool on which the import should be performed
+- `--region-new-pool` [_Optional_] - The user pool region the user pool on which the import should be performed _Default_: `us-east-1`
 - `-groups` [__Required__] - The group list on which the export/import should be performed
--- cognitoExport.py
+++ cognitoExport.py
@@ -17,8 +17,9 @@ GROUP = ''
 """ Parse All Provided Arguments """
 parser = argparse.ArgumentParser(description='Cognito User Pool export', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--user-pool-id', type=str, help="The current pool ID", required=True)
+parser.add_argument('--region-current-pool', type=str, help="The current pool region", required=True)
 parser.add_argument('--user-new-pool-id', type=str, help="The new pool ID", required=True)
-parser.add_argument('--region', type=str, help="The user pool region", required=True)
+parser.add_argument('--region-new-pool', type=str, help="The new pool region", required=True)
 parser.add_argument('-groups', '--export-groups', nargs='+', type=str, help="List of groups", required=True)
 parser.add_argument('--num-records', type=int, help="Max Number of Cognito Records to be exported")
 parser.add_argument('--profile', type=str, default='', help="The aws profile")
@@ -28,8 +29,10 @@ if args.user_pool_id:
     USER_POOL_ID = args.user_pool_id
 if args.user_new_pool_id:
     USER_NEW_POOL_ID = args.user_new_pool_id
-if args.region:
-    REGION = args.region
+if args.region_current_pool:
+    CURRENT_REGION = args.region_current_pool
+if args.region_new_pool:
+    NEW_REGION = args.region_new_pool
 if args.num_records:
     MAX_NUMBER_RECORDS = args.num_records   
 if args.profile:
@@ -39,9 +42,12 @@ if args.export_groups:
 
 if PROFILE:
     session = boto3.Session(profile_name=PROFILE)
-    client = session.client('cognito-idp', REGION)
+    client_current = session.client('cognito-idp', CURRENT_REGION)
+    client_new = session.client('cognito-idp', CURRENT_REGION)
 else:
-    client = boto3.client('cognito-idp', REGION)    
+    client_current = boto3.client('cognito-idp', CURRENT_REGION)
+    client_new = boto3.client('cognito-idp', CURRENT_REGION)
+
 
 def datetimeconverter(o):
     if isinstance(o, datetime.datetime):
@@ -68,12 +74,12 @@ while i < len(GROUPS):
             return user_record['phone_number']
             
     def importUsers(cognito_idp_cliend, next_pagination_token ='', Limit = LIMIT):  
-        return client.list_users_in_group(
+        return client_current.list_users_in_group(
             UserPoolId = USER_POOL_ID,
             GroupName = GROUPS[i],
             Limit = Limit,
             NextToken = next_pagination_token,
-        ) if next_pagination_token else client.list_users_in_group(
+        ) if next_pagination_token else client_current.list_users_in_group(
             UserPoolId = USER_POOL_ID,
             GroupName = GROUPS[i],
             Limit = Limit
@@ -86,7 +92,7 @@ while i < len(GROUPS):
         print(Fore.GREEN + "Creating user: " + user['Username'])
         attributes = filter(attributes_check, list(user["Attributes"]))
         try: 
-            return client.admin_create_user(
+            return client_new.admin_create_user(
                 UserPoolId=USER_NEW_POOL_ID,
                 Username=getUser(user),
                 UserAttributes=list(attributes),
@@ -94,25 +100,23 @@ while i < len(GROUPS):
                 ForceAliasCreation=False,
                 MessageAction='SUPPRESS',
             )
-        except client.exceptions.ClientError as err:
+        except client_new.exceptions.ClientError as err:
             error_message = err.response["Error"]["Message"]
             print(Fore.RED + "Error occured")
             print("Error Reason: " + error_message)
-            exit()
 
     def addUserToGroup(cognito_idp_cliend, user, group_name):
         print(Fore.GREEN + "Assing user: " + user['Username'] + " to group: " + group_name)
         try:
-            return client.admin_add_user_to_group(
+            return client_new.admin_add_user_to_group(
                 UserPoolId= USER_NEW_POOL_ID,
                 Username=getUser(user),
                 GroupName=group_name
             )
-        except client.exceptions.ClientError as err:
+        except client_new.exceptions.ClientError as err:
             error_message = err.response["Error"]["Message"]
             print(Fore.RED + "Error occured")
             print("Error Reason: " + error_message)
-            exit()
 
  
     pagination_counter = 0
@@ -122,11 +126,11 @@ while i < len(GROUPS):
     while pagination_token is not None:
         try:
             user_records = importUsers(
-                cognito_idp_cliend = client,
+                cognito_idp_cliend = client_current,
                 next_pagination_token = pagination_token,
                 Limit = LIMIT if LIMIT < MAX_NUMBER_RECORDS else MAX_NUMBER_RECORDS
             )
-        except client.exceptions.ClientError as err:
+        except client_current.exceptions.ClientError as err:
             #status = err.response["ResponseMetadata"]["HTTPStatusCode"]
             error_message = err.response["Error"]["Message"]
             print(Fore.RED + "Please Check your Cognito User Pool configs")
@@ -145,12 +149,12 @@ while i < len(GROUPS):
 
         for user in user_records['Users']:
             createUser(
-                cognito_idp_cliend = client,
+                cognito_idp_cliend =  client_new,
                 user = user
             )
 
             addUserToGroup(
-                cognito_idp_cliend = client, 
+                cognito_idp_cliend = client_new, 
                 user = user, 
                 group_name = GROUPS[i]
             )
