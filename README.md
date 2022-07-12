fa95eed19d98fb4483b99913c7e64dda332b3b16
Enrique Casanova <enrique.casanova@projekt202.com>
2022-07-12 17:36:11 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+## Instalation
+
+In order to use this script you should have Python 2 or Python 3 installed on your platform
+
+- run `pip install -r requirements.txt` (Python 2) or `pip3 install -r requirements.txt` (Python 3)
+
+## Run export
+
+To start export proccess you shout run next command (**Note**: use `python3` if you have Python 3 instaled)
+
+- `$ python3 CognitoExport.py --user-pool-id 'us-east-2_XXXXXXX' --user-new-pool-id 'us-east-2_XXXXXXXX' --region us-east-2 -groups TestGroup`
+
+### Script Arguments
+
+- `--user-pool-id` [__Required__] - The user pool ID for the user pool on which the export should be performed
+- `--region` [_Optional_] - The user pool region the user pool on which the export should be performed _Default_: `us-east-1`
+- `--user-new-pool-id` [__Required__] - The user pool ID for the user pool on which the import should be performed
+- `-groups` [__Required__] - The group list on which the export/import should be performed
---
+++ cognitoExport.py
@@ -0,0 +1,172 @@
+import boto3
+import json
+import datetime
+import time
+import sys
+import argparse
+from colorama import Fore
+
+REGION = ''
+USER_POOL_ID = ''
+LIMIT = 60
+MAX_NUMBER_RECORDS = 10000000
+REQUIRED_ATTRIBUTE = None
+PROFILE = ''
+GROUP = ''
+
+""" Parse All Provided Arguments """
+parser = argparse.ArgumentParser(description='Cognito User Pool export', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--user-pool-id', type=str, help="The current pool ID", required=True)
+parser.add_argument('--user-new-pool-id', type=str, help="The new pool ID", required=True)
+parser.add_argument('--region', type=str, help="The user pool region", required=True)
+parser.add_argument('-groups', '--export-groups', nargs='+', type=str, help="List of groups", required=True)
+parser.add_argument('--num-records', type=int, help="Max Number of Cognito Records to be exported")
+parser.add_argument('--profile', type=str, default='', help="The aws profile")
+args = parser.parse_args()
+
+if args.user_pool_id:
+    USER_POOL_ID = args.user_pool_id
+if args.user_new_pool_id:
+    USER_NEW_POOL_ID = args.user_new_pool_id
+if args.region:
+    REGION = args.region
+if args.num_records:
+    MAX_NUMBER_RECORDS = args.num_records   
+if args.profile:
+    PROFILE = args.profile            
+if args.export_groups:
+    GROUPS = list(args.export_groups) 
+
+if PROFILE:
+    session = boto3.Session(profile_name=PROFILE)
+    client = session.client('cognito-idp', REGION)
+else:
+    client = boto3.client('cognito-idp', REGION)    
+
+def datetimeconverter(o):
+    if isinstance(o, datetime.datetime):
+        return str(o)
+
+
+i = 0;
+while i < len(GROUPS):
+
+    def getUser(user):
+        usernames = list ()
+        user_record = {'username': user['Username'], 'email': None, 'phone_number': None}
+        for attr in user['Attributes']:
+            if attr['Name'] == 'email':
+                user_record['email'] = attr['Value']
+            if attr['Name'] == 'phone_number':
+                user_record['phone_number'] = attr['Value']
+            usernames.append (user_record)
+        
+        if user_record['email'] is not None:
+            return user_record['email']
+        
+        if user_record['phone_number'] is not None:
+            return user_record['phone_number']
+            
+    def importUsers(cognito_idp_cliend, next_pagination_token ='', Limit = LIMIT):  
+        return client.list_users_in_group(
+            UserPoolId = USER_POOL_ID,
+            GroupName = GROUPS[i],
+            Limit = Limit,
+            NextToken = next_pagination_token,
+        ) if next_pagination_token else client.list_users_in_group(
+            UserPoolId = USER_POOL_ID,
+            GroupName = GROUPS[i],
+            Limit = Limit
+        ) 
+
+    def attributes_check(attributes : list):
+        return attributes["Name"] != "sub"
+
+    def createUser(cognito_idp_cliend, user):
+        print(Fore.GREEN + "Creating user: " + user['Username'])
+        attributes = filter(attributes_check, list(user["Attributes"]))
+        try: 
+            return client.admin_create_user(
+                UserPoolId=USER_NEW_POOL_ID,
+                Username=getUser(user),
+                UserAttributes=list(attributes),
+                TemporaryPassword='Chang3me*',
+                ForceAliasCreation=False,
+                MessageAction='SUPPRESS',
+            )
+        except client.exceptions.ClientError as err:
+            error_message = err.response["Error"]["Message"]
+            print(Fore.RED + "Error occured")
+            print("Error Reason: " + error_message)
+            exit()
+
+    def addUserToGroup(cognito_idp_cliend, user, group_name):
+        print(Fore.GREEN + "Assing user: " + user['Username'] + " to group: " + group_name)
+        try:
+            return client.admin_add_user_to_group(
+                UserPoolId= USER_NEW_POOL_ID,
+                Username=getUser(user),
+                GroupName=group_name
+            )
+        except client.exceptions.ClientError as err:
+            error_message = err.response["Error"]["Message"]
+            print(Fore.RED + "Error occured")
+            print("Error Reason: " + error_message)
+            exit()
+
+ 
+    pagination_counter = 0
+    exported_records_counter = 0
+    pagination_token = ""
+   
+    while pagination_token is not None:
+        try:
+            user_records = importUsers(
+                cognito_idp_cliend = client,
+                next_pagination_token = pagination_token,
+                Limit = LIMIT if LIMIT < MAX_NUMBER_RECORDS else MAX_NUMBER_RECORDS
+            )
+        except client.exceptions.ClientError as err:
+            #status = err.response["ResponseMetadata"]["HTTPStatusCode"]
+            error_message = err.response["Error"]["Message"]
+            print(Fore.RED + "Please Check your Cognito User Pool configs")
+            print("Error Reason: " + error_message)
+            exit()
+        except:
+            print(Fore.RED + "Something else went wrong:")
+            print(Fore.RED + err.response["Error"]["Message"])
+            exit()     
+
+        """ Check if there next paginatioon is exist """
+        if set(["PaginationToken","NextToken"]).intersection(set(user_records)):
+            pagination_token = user_records['PaginationToken'] if "PaginationToken" in user_records else user_records['NextToken']
+        else:
+            pagination_token = None
+
+        for user in user_records['Users']:
+            createUser(
+                cognito_idp_cliend = client,
+                user = user
+            )
+
+            addUserToGroup(
+                cognito_idp_cliend = client, 
+                user = user, 
+                group_name = GROUPS[i]
+            )
+
+            exported_records_counter +=  1
+           
+        """ Display Proccess Infor """
+        pagination_counter += 1
+        print(Fore.YELLOW + 'Total '+ GROUPS[i] +' Exported Records: ' + str(exported_records_counter))
+
+        if MAX_NUMBER_RECORDS and exported_records_counter >= MAX_NUMBER_RECORDS:
+            print(Fore.GREEN + "INFO: Max Number of Exported  "+GROUPS[i]+" Reached")
+            break    
+
+        """ Cool Down before next batch of Cognito Users """
+        time.sleep(0.15)
+   
+    i = i + 1;   
+
