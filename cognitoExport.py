af70eab2e86ed11f7401f9584abf83179ef8893e
Enrique Casanova <enrique.casanova@projekt202.com>
2022-07-26 15:19:45 -0500

    Change required profiles

--- cognitoExport.py
+++ cognitoExport.py
@@ -51,8 +51,8 @@ parser.add_argument('--region-current-pool', type=str, default="us-east-1", help
 parser.add_argument('--user-new-pool-id', type=str, help="The new pool ID for perform the import", required=True)
 parser.add_argument('--region-new-pool', type=str, default="us-east-1", help="The new pool region for perform the import", required=False)
 parser.add_argument('--groups', nargs='+', type=str, help="List of user groups", required=True)
-parser.add_argument('--profile-current-pool', type=str, default='default', help="The aws profile for perform the export", required=True)
-parser.add_argument('--profile-new-pool', type=str, default='default', help="The aws profile for perform the import", required=True)
+parser.add_argument('--profile-current-pool', type=str, default='default', help="The aws profile for perform the export", required=False)
+parser.add_argument('--profile-new-pool', type=str, default='default', help="The aws profile for perform the import", required=False)
 parser.add_argument('--new-password', default='Chang3me*', type=str, help="The new password for the users in the new pool", required=False)
 parser.add_argument('--num-records', type=int, help="Max Number of Cognito Records to be exported")
 
